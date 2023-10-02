ee517f447a15e859b764f29503a378705eb991e5
LY-IENG Steven <steven.ly412@gmail.com>
2023-10-02 20:51:20 +0200

    link technologies

---
+++ src/main/java/fr/uge/gitclout/gitclout/GitcloutApplication.java
@@ -0,0 +1,13 @@
+package fr.uge.gitclout.gitclout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcloutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitcloutApplication.class, args);
+	}
+
+}
