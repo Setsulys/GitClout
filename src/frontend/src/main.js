8bec0b8c0a256c729b3f0fa4e34ea8c90ab00e23
LY STEVEN <steven.ly@heckel02.u-pem.fr>
2023-11-10 14:24:39 +0100

    add semantic ui

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-//import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css';
 //import Vue from 'vue';
 //import SuiVue from 'semantic-ui-vue';
 //import '../semantic/dist/semantic.min.css';
